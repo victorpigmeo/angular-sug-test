22de008a2262ad5effb60f6c7aa186dfa386f067
Victor Carvalho <victor.blq@gmail.com>
2020-06-15 09:38:30 -0300

    Fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'https://angularkt-aabe.restdb.io/rest/cats',
+  serverBaseUrl: 'https://angularkt-aabe.restdb.io/rest',
   apiKey: '5ee270ea4e6043315b0af9cb'
 };
